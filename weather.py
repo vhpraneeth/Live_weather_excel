fc9867ab6ccb1a17c49c1b3037921de45a1d1b35
Praneeth <praneeth.18bce7147@vitap.ac.in>
2020-08-21 13:14:02 +0000

    Fixed mistake

--- weather.py
+++ weather.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 print('Process started')
 df = pd.read_excel('Sheet.xlsx')
-url = 'http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q={}'  # 0760663bdd9e29d3ee8fd65cf59197e9
+main_url = 'http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q={}'  # 0760663bdd9e29d3ee8fd65cf59197e9
 
 while 1:  # repeat continuously
     new_df = []
@@ -13,14 +13,16 @@ while 1:  # repeat continuously
         if row[-1]:  # if Update is 1
             city = row[0]
             unit = row[3]
-            url = url.format(city)
+            url = main_url.format(city)
             data = requests.get(url).json()
             temp = data['main']['temp'] - 273.15  # temperature in C  - recheck values
             if 'F' in unit:
                 temp = (temp * 1.8) + 32  # to F
+            row[1] = temp
             row[2] = data['main']['humidity']
         new_df.append(row)
+        print(row)
     new_df = pd.DataFrame(new_df, columns=df.keys())
     new_df.to_excel('Sheet.xlsx', index=False)
-    # new_df.to_csv('Sheet.csv', index=False)
+    new_df.to_csv('Sheet.csv', index=False)
     time.sleep(1)
