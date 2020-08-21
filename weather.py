f3154027d50cd8d5e73f5b28bfcbadb3617e77c5
Praneeth <praneeth.18bce7147@vitap.ac.in>
2020-08-21 10:48:11 +0000

    Files updated and tested

--- weather.py
+++ weather.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
-
+import time
 import requests
+import pandas as pd
 
+print('Process started')
+df = pd.read_excel('Sheet.xlsx')
+url = 'http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q={}'  # 0760663bdd9e29d3ee8fd65cf59197e9
 
-
-url = "http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q={}"
-city = "vijayawada"
-url = url.format(city)
-json_data = requests.get(url).json()           # get weather
-formatted_data = json_data['weather'][0]['main'].lower()
-tmp = int(json_data["main"]["temp"] - 273.15)
-
+while 1:  # repeat continuously
+    new_df = []
+    for row in df.values:
+        if row[-1]:  # if Update is 1
+            city = row[0]
+            unit = row[3]
+            url = url.format(city)
+            data = requests.get(url).json()
+            temp = data['main']['temp'] - 273.15  # temperature in C
+            if 'F' in unit:
+                temp = (temp * 1.8) + 32  # to F
+            row[2] = data['main']['humidity']
+        new_df.append(row)
+    new_df = pd.DataFrame(new_df, columns=df.keys())
+    new_df.to_excel('Sheet.xlsx', index=False)
+    # new_df.to_csv('Sheet.csv', index=False)
+    time.sleep(1)
