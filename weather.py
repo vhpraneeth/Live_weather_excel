14427cde8785ec0f4197b595a02794890f5b786d
Praneeth <praneeth.18bce7147@vitap.ac.in>
2020-08-22 04:52:11 +0000

    $1

--- weather.py
+++ weather.py
@@ -18,8 +18,7 @@ while 1:  # repeat continuously
             temp = data['main']['temp'] - 273.15  # temperature in C  - recheck values
             if 'F' in unit:
                 temp = (temp * 1.8) + 32  # to F
-            temp = str(temp)
-            temp = temp[:temp.find('.')+2]
+            temp = '{:.2f}'.format(temp)
             row[1] = temp
             row[2] = data['main']['humidity']
         new_df.append(row)
